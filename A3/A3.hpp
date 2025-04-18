fe24bcd8c7f5b3db35748343abfdaee0a1784b2c
Winroth13 <emil.winroth@gmail.com>
2025-04-18 17:02:33 +0200

    A3 Complete

--- A3/A3.hpp
+++ A3/A3.hpp
@@ -39,14 +39,13 @@ int DigitNr(T element, int divisor, int modulus) {
 }
 
 template <class T>
-void InnerCountingSort(T elements[], int nrOfElements, int digit)
+void InnerCountingsort(T elements[], int nrOfElements, int digit)
 {
 	T* B = new T[nrOfElements];
 	int max = 10;
 	int* C = new int[max] {0};
 	int modulus = 1;
 	int divisor = 1;
-
 
 	for (int i = 1; i < digit; ++i) {
 		divisor *= 10;
@@ -81,20 +80,104 @@ void RadixsortCounting(T elements[], int nrOfElements)
 	int nrDigits = std::to_string(max).length();
 
 	for (int d = 1; d < nrDigits + 1; ++d) {
-		InnerCountingSort(elements, nrOfElements, d);
+		InnerCountingsort(elements, nrOfElements, d);
 	}
 }
 
+template <class T>
+void InnerBucketsort(T elements[], int nrOfElements, int digit)
+{
+	T* B[10];
+	int count[10] = { 0 };
+	int modulus = 1;
+	int divisor = 1;
+
+	for (int i = 0; i < 10; ++i) {
+		B[i] = new T[nrOfElements];
+	}
+
+	for (int i = 1; i < digit; ++i) {
+		divisor *= 10;
+	}
+	modulus = divisor * 10;
+
+	for (int i = 0; i < nrOfElements; ++i) {
+		B[DigitNr(elements[i], divisor, modulus)][count[DigitNr(elements[i], divisor, modulus)]] = elements[i];
+		count[DigitNr(elements[i], divisor, modulus)]++;
+	}
+
+	int position = 0;
+
+	for (int i = 0; i < 10; ++i) {
+		for (int j = 0; j < count[i]; ++j) {
+			elements[position] = B[i][j];
+			position++;
+		}
+	}
+
+	for (int i = 0; i < 10; ++i) {
+		delete[] B[i];
+	}
+}
 
 template <class T>
 void RadixsortBuckets(T elements[], int nrOfElements)
 {
+	T max = *(std::max_element(elements, elements + nrOfElements));
+	int nrDigits = std::to_string(max).length();
 
+	for (int d = 1; d < nrDigits + 1; ++d) {
+		InnerBucketsort(elements, nrOfElements, d);
+	}
+}
+
+int LetterIndex(std::string element, int letterNr) {
+	if (element.length() < letterNr) {
+		return 0;
+	}
+	else {
+		return (int)element[element.length() - letterNr] - 96;
+	}
+}
+
+void InnerStringBucketsort(std::string elements[], int nrOfElements, int letterNr)
+{
+	std::string* B[27];
+	int count[27] = { 0 };
+	int modulus = 1;
+	int divisor = 1;
+
+	for (int i = 0; i < 27; ++i) {
+		B[i] = new std::string[nrOfElements];
+	}
+
+	for (int i = 0; i < nrOfElements; ++i) {
+		B[LetterIndex(elements[i], letterNr)][count[LetterIndex(elements[i], letterNr)]] = elements[i];
+		count[LetterIndex(elements[i], letterNr)]++;
+	}
+
+	int position = 0;
+
+	for (int i = 0; i < 27; ++i) {
+		for (int j = 0; j < count[i]; ++j) {
+			elements[position] = B[i][j];
+			position++;
+		}
+	}
+
+	for (int i = 0; i < 27; ++i) {
+		delete[] B[i];
+	}
 }
 
 void RadixsortStrings(std::string elements[], int nrOfElements)
 {
+	std::string max = *(std::max_element(elements, elements + nrOfElements, [](const std::string& i, const std::string& j) {return i.length() < j.length(); }));
+	int nrLetters = max.length();
 
+	for (int n = 1; n < nrLetters + 1; ++n) {
+		InnerStringBucketsort(elements, nrOfElements, n);
+	}
 }
 
 #endif
--- A3/test.cpp
+++ A3/test.cpp
@@ -9,8 +9,24 @@ int main() {
 	}*/
 	int arr2[10] = { 41, 63, 23, 64, 78, 99, 100, 52, 60, 5 };
 
-	RadixsortCounting(arr2, 10);
+	/*RadixsortCounting(arr2, 10);
 	for (int i = 0; i < 10; ++i) {
 		std::cout << arr2[i] << ", ";
+	}*/
+
+	/*RadixsortBuckets(arr2, 10);
+	for (int i = 0; i < 10; ++i) {
+		std::cout << arr2[i] << ", ";
+	}*/
+
+	std::string arr3[10] = { "abc", "aaa", "bbb", "ccc", "ff", "aaaa", "a", "ki", "di", "fa" };
+	RadixsortStrings(arr3, 10);
+	for (int i = 0; i < 10; ++i) {
+		std::cout << arr3[i] << ", ";
 	}
+
+	/*std::string test[27] = { "", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z" };
+	for (int i = 0; i < 27; ++i) {
+		std::cout << LetterIndex(test[i], 1) << std::endl;
+	}*/
 }
